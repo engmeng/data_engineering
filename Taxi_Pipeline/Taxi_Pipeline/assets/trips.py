048376d0b6c4eb45240d647371d93558ad48914f
Eng Meng Tan <eng_meng@hotmail.com>
2025-05-26 20:56:32 +0200

    Added code to trips.py to allow for grabbing of information via http request. Modified definitions.py to account for new file structure

--- Taxi_Pipeline/Taxi_Pipeline/assets/trips.py
+++ Taxi_Pipeline/Taxi_Pipeline/assets/trips.py
@@ -4,3 +4,32 @@ import dagster as dg
 import duckdb
 import os
 import pandas as pd
+
+@dg.asset(
+  group_name="raw_files"
+  )
+def taxi_trips_file(context: dg.AssetExecutionContext) -> dg.MaterializeResult:
+    """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    """
+    # Choose the month to fetch from the URL
+    month_to_fetch = '2023-03'
+    #partition_date_str = context.partition_key
+    #month_to_fetch = partition_date_str[:-3]
+    
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+    
+    
+    # Save the parquet file 
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+        
+    num_rows = len(pd.read_parquet(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)))
+
+    return dg.MaterializeResult(
+      metadata={
+                'Number of records': dg.MetadataValue.int(num_rows)
+            }
+        )
--- Taxi_Pipeline/Taxi_Pipeline/definitions.py
+++ Taxi_Pipeline/Taxi_Pipeline/definitions.py
@@ -1,11 +1,10 @@
 import dagster as dg
 from dagster import Definitions, load_assets_from_modules
 
-from data_engineering.Taxi_Pipeline.Taxi_Pipeline.assets import trips 
+from Taxi_Pipeline.assets import trips 
 
 trip_assets = dg.load_assets_from_modules([trips])
 
 defs = Definitions(
     assets=trip_assets,
-    group_name="Raw Loading"
 )
